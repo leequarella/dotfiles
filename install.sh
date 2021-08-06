7e16320b2f7423fbf383c4d8af09e4c4149eef45
Lee Quarella <leequarella@gmail.com>
2021-08-06 14:19:41 -0400

    Playing around for codespaces

--- install.sh
+++ install.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
 #Copy basics
-  echo "Copying .zshrc"
-  cp .zshrc $HOME
+  # echo "Copying .zshrc"
+  # cp .zshrc $HOME
 
-  echo "Copying .powerlevel9k"
-  cp .powerlevel9k $HOME
+  # echo "Copying .powerlevel9k"
+  # cp .powerlevel9k $HOME
 
   echo "Copying .vimrc"
-  cp .vimrc $HOME
+  ln -s $dotfiles_root/vimrc ~/.vim/vimrc
 
-  echo "Copying .bash_aliases"
-  cp bash/.bash_aliases $HOME
+  # echo "Copying .bash_aliases"
+  # cp bash/.bash_aliases $HOME
 
   echo "Copying .bashrc"
   cp bash/.bashrc $HOME
+  ln -s $dotfiles_root/bashrc ~/.bash/bashrc
 
-  echo "Copying .tmux.conf"
-  cp .tmux.conf $HOME
+  # echo "Copying .tmux.conf"
+  # cp .tmux.conf $HOME
 
 #Copy bash correct bash profile based on OS
-  case $( uname -s ) in
-  Linux)
-    echo "Copying Linux Bash Profile"
-    cp bash/.bash_profile-ubuntu "$HOME/.bash_profile";;
-  *)
-    echo "Copying Mac Bash Profile"
-    cp bash/.bash_profile-mac "$HOME/.bash_profile";;
-  esac
+  # case $( uname -s ) in
+  # Linux)
+  #   echo "Copying Linux Bash Profile"
+  #   cp bash/.bash_profile-ubuntu "$HOME/.bash_profile";;
+  # *)
+  #   echo "Copying Mac Bash Profile"
+  #   cp bash/.bash_profile-mac "$HOME/.bash_profile";;
+  # esac
 
 #Copy .gitconfig
-  echo "Copying .gitconfig"
-  cp .gitconfig $HOME
+  # echo "Copying .gitconfig"
+  # cp .gitconfig $HOME
   # read -p "Full Name: " full_name
   # read -p "Github Login(email): " git_email
   # git config --global user.name "$full_name"
